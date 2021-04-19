d7f184a584b204f4b6165cfdf0d53928ee657001
Oran9eUtan <Oran9eUtan@gmail.com>
2021-04-19 18:32:28 +0200

    fix: Wrong imports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export {MinecraftModel, validateMinecraftModel} from './model'
+export {MinecraftModel, ModelParseError} from './model'
 export {MinecraftModelMesh, MinecraftModelLoader} from './mesh'
 export {MinecraftTexture, MinecraftTextureLoader} from './texture'
