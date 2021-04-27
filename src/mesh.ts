e369fe94d50401564fdb24da5ec0208201a87842
Oran9eUtan <Oran9eUtan@gmail.com>
2021-04-27 21:26:09 +0200

    feat: Added "calculateCommonAnimationPeriod"

--- src/mesh.ts
+++ src/mesh.ts
@@ -2,7 +2,6 @@ import { Mesh } from 'three'
 import { ElementGeometry } from './geometry'
 import { MinecraftModelMaterial } from './material'
 import { MISSING_TEXTURE, MinecraftTexture } from './texture'
-import { lcm } from './utils'
 
 export class ElementMesh extends Mesh {
     private textureToMaterialMap: { [texturePath: string]: MinecraftModelMaterial }
@@ -26,21 +25,5 @@ export class ElementMesh extends Mesh {
         for (const texturePath in this.textureToMaterialMap) {
             this.textureToMaterialMap[texturePath]!.map = resolver(texturePath) ?? MISSING_TEXTURE
         }
-    }
-
-    public isAnimated() {
-        return Object.values(this.textureToMaterialMap).map(m => m.map).some(t=> (t as MinecraftTexture | undefined)?.isAnimated())
-    }
-
-    public setAnimationFrame(index: number) {
-        for(const texture of Object.values(this.textureToMaterialMap)) {
-            (texture.map as MinecraftTexture).setAnimationFrame(index);
-        }
-    }
-
-    public getAnimationPeriod() {
-        return Object.values(this.textureToMaterialMap)
-            .map(m => (m.map as MinecraftTexture)?.numFrames() ?? 1)
-            .reduce((prev, current) => lcm(prev, current), 1)
     }
 }
--- src/texture.ts
+++ src/texture.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { lcm } from './utils'
 
 export const CHECKERBOARD_IMAGE = new THREE.ImageLoader().load(
     'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAAACXBIWXMAAC4jAAAuIwF4pT92AAAAB3RJTUUH4goSFSEEtucn/QAAABl0RVh0Q29tbWVudABDcmVhdGVkIHdpdGggR0lNUFeBDhcAAAAkSURBVCjPY2TAAX4w/MAqzsRAIhjVQAxgxBXeHAwco6FEPw0A+iAED8NWwMQAAAAASUVORK5CYII=',
@@ -37,6 +38,15 @@ export class MinecraftTexture extends THREE.Texture {
     numFrames() {
         return this.numTiles
     }
+}
+
+/**
+ * Calculates period after which all texture animations repeat
+ */
+export function calculateCommonAnimationPeriod(textures: MinecraftTexture[]) {
+    return textures
+        .map(t => t.numFrames())
+        .reduce((prev, current) => lcm(prev, current), 1);
 }
 
 export const MISSING_TEXTURE = new MinecraftTexture()
