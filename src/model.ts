a5b6bc687bf40567a45472003c0e6d67cc056aee
Oran9eUtan <Oran9eUtan@gmail.com>
2021-05-01 00:19:30 +0200

    refactor: Removed obsolete HierarchicalModelResolver

--- src/model.ts
+++ src/model.ts
@@ -1,7 +1,11 @@
 // Types for minecraft json models (see https://minecraft.fandom.com/wiki/Model)
 
 import { Loader, FileLoader } from 'three'
-import { MinecraftModel, validateMinecraftModelJson, MinecraftModelJson } from '@oran9e/minecraft-model'
+import {
+    MinecraftModel,
+    validateMinecraftModelJson,
+    MinecraftModelJson,
+} from '@oran9e/minecraft-model'
 
 export class MinecraftModelLoader extends Loader {
     public async load(url: string) {
@@ -12,48 +16,5 @@ export class MinecraftModelLoader extends Loader {
         const data = await loader.loadAsync(url)
         validateMinecraftModelJson(data)
         return MinecraftModel.fromJson(data as MinecraftModelJson)
-    }
-}
-
-export class HierarchicalModelResolver {
-    private hierarchy: MinecraftModel[];
-
-    constructor(model: MinecraftModel, ancestors: {[assetPath: string]: MinecraftModel}) {
-        this.hierarchy = this.createHierarchy(model, ancestors);
-    }
-
-    get elements() {
-        for(const model of this.hierarchy) {
-            if(model.elements != null && model.elements.length >= 1) {
-                return model.elements;
-            }
-        }
-        return undefined;
-    }
-
-    get textures() {
-        let textures = {};
-        for(let i = this.hierarchy.length-1; i >= 0; i--) {
-            const model = this.hierarchy[i]!;
-            if(model.textures != null) {
-                Object.assign(textures, model.textures);
-            }
-        }
-        return textures;
-    }
-
-    private createHierarchy(root: MinecraftModel, ancestors: {[assetPath: string]: MinecraftModel}) {
-        let hierarchy = [root];
-        let current = root;
-        while(current.parent != null) {
-            const parent = ancestors[current.parent];
-            if(parent != null) {
-                hierarchy.push(parent);
-                current = parent;
-            } else {
-                break;
-            }
-        }
-        return hierarchy;
     }
 }
