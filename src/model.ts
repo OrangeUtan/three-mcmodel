abc26bff49d7d2093716bbde12ba5c5496a658f8
Oran9eUtan <Oran9eUtan@gmail.com>
2021-05-01 10:59:29 +0200

    fix: Removed obsolete MinecraftModelLoader

--- src/model.ts
+++ src/model.ts
@@ -2,22 +2,9 @@
 
 import { Loader, FileLoader } from 'three'
 import {
-    MinecraftModel,
     validateMinecraftModelJson,
     MinecraftModelJson,
 } from '@oran9e/minecraft-model'
-
-export class MinecraftModelLoader extends Loader {
-    public async load(url: string) {
-        const loader = new FileLoader(this.manager)
-        loader.setPath(this.path)
-        loader.setResponseType('json')
-
-        const data = await loader.loadAsync(url)
-        validateMinecraftModelJson(data)
-        return MinecraftModel.fromJson(data as MinecraftModelJson)
-    }
-}
 
 export class MinecraftModelJsonLoader extends Loader {
     public async load(url: string) {
